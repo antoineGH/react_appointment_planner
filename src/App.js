473dee316e1c6f9d46c549cb196e5aa751b56250
Templars <antoine.ratat@gmail.com>
2021-08-29 16:20:02 +0800

    cra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route, Redirect, NavLink } from 'react-router-dom'
+
+import AppointmentsPage from './containers/appointmentsPage/AppointmentsPage'
+import ContactsPage from './containers/contactsPage/ContactsPage'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const ROUTES = {
+		CONTACTS: '/contacts',
+		APPOINTMENTS: '/appointments',
+	}
+
+	return (
+		<>
+			<nav>
+				<NavLink to={ROUTES.CONTACTS} activeClassName='active'>
+					Contacts
+				</NavLink>
+				<NavLink to={ROUTES.APPOINTMENTS} activeClassName='active'>
+					Appointments
+				</NavLink>
+			</nav>
+			<main>
+				<Switch>
+					<Route path={ROUTES.CONTACTS}>
+						<ContactsPage />
+					</Route>
+					<Route path={ROUTES.APPOINTMENTS}>
+						<AppointmentsPage />
+					</Route>
+				</Switch>
+			</main>
+		</>
+	)
 }
 
-export default App;
+export default App
---
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function AppointmentsPage() {
+	const handleSubmit = (e) => {
+		e.preventDefault()
+	}
+
+	return (
+		<div>
+			<section>
+				<h2>Add Appointment</h2>
+			</section>
+			<hr />
+			<section>
+				<h2>Appointments</h2>
+			</section>
+		</div>
+	)
+}
